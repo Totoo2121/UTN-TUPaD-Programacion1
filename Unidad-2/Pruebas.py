4571962198d57546e0beaff3f6ce7ac131756b52
Totoo2121 <tomasmartinez21507@gmail.com>
2025-08-24 22:22:22 -0300

    Ejercicios tp2
    Ejercicios no terminados tp2

---
+++ Unidad-2/Pruebas.py
@@ -0,0 +1,6 @@
+#Ejercicio 3
+numero = int(input("Ingrese un numero"))
+if numero % 2 == 0:
+    print("Ha ingresado un numero par")
+else:
+    print("Por favor ingrese un numero par")
---
+++ Unidad-2/TP-2.py
@@ -0,0 +1,20 @@
+#Ejercicio 1
+edad = int(input("Ingrese su edad"))
+if edad > 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+    
+#Ejercicio 2
+nota = int(input("Ingrese su nota"))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
+
+#Ejercicio 3
+numero = int(input("Ingrese un numero"))
+if numero % 2 == 0:
+    print("Ha ingresado un numero par")
+else:
+    print("Por favor ingrese un numero par")
