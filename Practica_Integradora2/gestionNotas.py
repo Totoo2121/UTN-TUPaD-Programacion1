5c563942b76a6fbb01770b1d1a12a5514251d119
Totoo2121 <tomasmartinez21507@gmail.com>
2025-10-23 18:02:08 -0300

    terminado

--- Practica_Integradora2/gestionNotas.py
+++ Practica_Integradora2/gestionNotas.py
@@ -1,3 +1,40 @@
+def pedir_entero(mensaje):
+    while True:
+        try:
+            valor = int(input(mensaje))
+            return valor
+        except ValueError:
+            print("ERROR: Debe ingresar un número válido.")
+
+
+
+def calcular_promedio_general():
+    for 
+    notasFinales = 
+
+
+
+
+def nota_mas_alta(materiasCargadas):
+    for alumno, materias in materiasCargadas:
+        print(f"\nAlumno: {alumno}")
+        materia_top = ""
+        nota_top = -1
+
+        for materia in materias:
+            if materia[3] > nota_top:   
+                nota_top = materia[3]   
+                materia_top = materia[0] 
+
+        print(f"La materia con la nota mas alta es {materia_top} el promedio es {nota_top}")
+
+
+
+
+
+
+
+
 alumnos = {60902 : "Rodolfo Fernandez",61654 : "Luis Gomez",61852: "Andrea Pereira", 61754 : "Juan Cruz Gonzales"}
 materias = [["Ciencias", 0, 0,0 ],["Historia",0 ,0 ,0],["Geografia",0 ,0 ,0 ],["Matematica",0 ,0 ,0 ],["Fisica",0 ,0 ,0 ]]
 notasFinales = [["Rodolfo Fernandez",0],["Luis Gomez",0],["Andrea Pereira",0],["Juan Cruz Gonzales",0]]
@@ -5,20 +42,25 @@ materiasCargadas = []
 def pedirNotas(materias):
     for materia in materias:
         while True:
-            nota1 = int(input(f"Ingrese Nota 1 para {materia[0]}: "))
+            nota1 = pedir_entero(f"Ingrese Nota 1 para {materia[0]}: ")
             if 0 <= nota1 <= 10:
                 break
             print("Ingrese una nota valida entre 0 y 10")
         materia[1] = nota1
 
         while True:
-            nota2 = int(input(f"Ingrese Nota 2 para {materia[0]}: "))
+            nota2 = pedir_entero(f"Ingrese Nota 2 para {materia[0]}: ")
             if 0 <= nota2 <= 10:
                 break
             print("Ingrese una nota valida entre 0 y 10")
         materia[2] = nota2
         promedio = (nota1 + nota2) / 2
         materia[3] = promedio
+        
+
+
+
+
 for i in alumnos:
     print(f"Legajo = {i} Nombre = {alumnos[i]}")
     materiasAlumno = [
@@ -28,6 +70,9 @@ for i in alumnos:
         ["Matematica", 0, 0, 0],
         ["Fisica", 0, 0, 0]
     ]
+    
+    
+    
     pedirNotas(materiasAlumno)
     materiasCargadas.append([alumnos[i], materiasAlumno])
 for alumno, materias in materiasCargadas:
@@ -35,3 +80,4 @@ for alumno, materias in materiasCargadas:
     for materia in materias:
         print(materia)
 
+    nota_mas_alta(materiasCargadas)
